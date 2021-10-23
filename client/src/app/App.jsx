15a51a296ce239a28bf07bb1420e99335044d538
Craig <indigomx9@gmail.com>
2021-10-22 20:21:45 -0400

    Added the React front-end.

---
+++ client/src/app/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos error vitae optio cum voluptatem repudiandae eligendi tempore expedita alias ratione ducimus atque ipsum quod, magni ea cupiditate provident impedit perspiciatis.</p>
+        </React.Fragment>
+    );
+};
+
+
+
+
+
---
+++ client/src/components/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const List = () => {
+    return (
+        <React.Fragment>
+            <h1>List</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos error vitae optio cum voluptatem repudiandae eligendi tempore expedita alias ratione ducimus atque ipsum quod, magni ea cupiditate provident impedit perspiciatis.</p>
+        </React.Fragment>
+    );
+};
+
+
+
+
+
