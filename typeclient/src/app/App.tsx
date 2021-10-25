2ca59e324115385cea33bb646d3d3be962afe703
Craig <indigomx9@gmail.com>
2021-10-24 21:15:01 -0400

    Added React with TypeScript.

---
+++ typeclient/src/app/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, nam amet. Delectus dolores quidem aut ipsa nostrum a. A recusandae quia sint laboriosam ab illum eum inventore omnis vel modi!</p>
+        </React.Fragment>
+    );
+};
+
+
+
+
